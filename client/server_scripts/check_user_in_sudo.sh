6c22a7372d1eb3e18754f1ea222fc8ebb21a5c36
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-10-27 13:09:41 +0400

    Checking requirements in script
    Checking requirements for sudo users in script

--- client/server_scripts/check_user_in_sudo.sh
+++ client/server_scripts/check_user_in_sudo.sh
@@ -1,2 +1,3 @@
+echo $LC_MESSAGES | grep -qE "en_US.UTF-8|C.UTF-8" || export LC_MESSAGES=C.UTF-8;\
 CUR_USER=$(whoami);\
-groups $CUR_USER
+groups $CUR_USER | grep sudo && sudo -n -u $CUR_USER sh -c "sudo -n uname 2>&1 > /dev/null"
