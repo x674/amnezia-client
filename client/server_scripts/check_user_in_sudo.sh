ae681ad6d2496395303ce2d1e14c4d80de079ece
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-10-27 15:33:08 +0400

    simplification

--- client/server_scripts/check_user_in_sudo.sh
+++ client/server_scripts/check_user_in_sudo.sh
@@ -1,3 +1,3 @@
 echo $LC_MESSAGES | grep -qE "en_US.UTF-8|C.UTF-8" || export LC_MESSAGES=C.UTF-8;\
 CUR_USER=$(whoami);\
-groups $CUR_USER | grep sudo && sudo -n -u $CUR_USER sh -c "sudo -n uname 2>&1 > /dev/null"
+groups $CUR_USER | grep sudo && sudo -nu $CUR_USER sudo -n uname > /dev/null
