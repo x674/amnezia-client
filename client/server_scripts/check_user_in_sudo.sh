3dd2fad6aa8c56b7b46a8f2c1e3687ff9f58dad2
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-11-16 12:49:43 +0400

    checking sudo permission for root
    Сhecking sudo permission for root.
    Сhanging the verification method 
    and redefining the system language.
    Сommand to use home directory name if whoami returns error or is missing.

--- client/server_scripts/check_user_in_sudo.sh
+++ client/server_scripts/check_user_in_sudo.sh
@@ -1,3 +1,5 @@
-echo $LC_MESSAGES | grep -qE "en_US.UTF-8|C.UTF-8" || export LC_MESSAGES=C.UTF-8;\
-CUR_USER=$(whoami);\
-groups $CUR_USER | grep sudo && sudo -nu $CUR_USER sudo -n uname > /dev/null
+CUR_USER=$(whoami 2> /dev/null || echo ~ | sed 's/.*\///');\
+groups $CUR_USER;\
+echo $LANG | grep -qE "en_US.UTF-8|C.UTF-8" || export LC_ALL=C;\
+if [ "$CUR_USER" = "root" ] && command -v sudo > /dev/null 2>&1; then sudo -nu $CUR_USER sudo -n uname > /dev/null;\
+else [ "$CUR_USER" != "root" ] && groups $CUR_USER | grep -qE " sudo| wheel" && sudo -nu $CUR_USER sudo -n uname > /dev/null; fi
