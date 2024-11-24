4d2d7e4071f428f4dfe06853a691f5855c3baa46
albexk <albexk@proton.me>
2024-11-24 21:18:35 +0300

    fix: add the touch emulation method for Android TV

--- client/ui/controllers/systemController.cpp
+++ client/ui/controllers/systemController.cpp
@@ -134,3 +134,10 @@ bool SystemController::isAuthenticated()
     return true;
 #endif
 }
+
+void SystemController::sendTouch(float x, float y)
+{
+#ifdef Q_OS_ANDROID
+    AndroidController::instance()->sendTouch(x, y);
+#endif
+}
