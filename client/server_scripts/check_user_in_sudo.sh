055c8f7b668a11df474e1fe6cbf2de738c0c58ff
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-12-09 08:51:55 +0400

    splitting the sudo check script
    The sudo command check script is split to check permissions correctly

--- client/server_scripts/check_user_in_sudo.sh
+++ client/server_scripts/check_user_in_sudo.sh
@@ -1,3 +1,3 @@
 CUR_USER=$(whoami 2> /dev/null || echo ~ | sed 's/.*\///');\
 echo $LANG | grep -qE "en_US.UTF-8|^C.UTF-8" || export LC_ALL=C;\
-[ "$CUR_USER" = "root" ] || groups $CUR_USER | grep -E "\<sudo\>|\<wheel\>" && sudo -nu $CUR_USER sudo -n uname > /dev/null
+[ "$CUR_USER" = "root" ] || groups $CUR_USER | grep -E "\<sudo\>|\<wheel\>" && sudo -nu $CUR_USER uname > /dev/null && sudo -n uname > /dev/null
