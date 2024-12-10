ce4cb28e747ac63bb206900ebe9744a5edf571a2
lunardunno <126363523+lunardunno@users.noreply.github.com>
2024-12-10 05:44:55 +0400

    improved script readability

--- client/server_scripts/check_user_in_sudo.sh
+++ client/server_scripts/check_user_in_sudo.sh
@@ -1,5 +1,5 @@
 CUR_USER=$(whoami 2> /dev/null || echo ~ | sed 's/.*\///');\
-echo $LANG | grep -qE "en_US.UTF-8|^C.UTF-8" || export LC_ALL=C;\
-if [ "$CUR_USER" = "root" ] || groups $CUR_USER | grep -E "\<sudo\>|\<wheel\>";\
-then sudo -nu $CUR_USER uname > /dev/null && sudo -n uname > /dev/null;\
+echo $LANG | grep -qE "^(en_US.UTF-8|C.UTF-8|C)$" || export LC_ALL=C;\
+if groups $CUR_USER | grep -E "\<(sudo|wheel)\>" || [ "$CUR_USER" = "root" ]; then \
+  sudo -nu $CUR_USER uname > /dev/null && sudo -n uname > /dev/null;\
 fi
